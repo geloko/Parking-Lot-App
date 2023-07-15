bef0b1047b9e12a3521d7579c034dec270b8f84e
geloko <geloko@users.noreply.github.com>
2023-07-16 02:18:37 +0800

    chore: add admin, urls and base viewset

---
+++ backend/parking/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from django.conf.urls import url
+from django.urls import include
+
+import views
+
+router = DefaultRouter()
+
+urlpatterns = [
+  url(r'', include(router.urls))
+]
--- backend/parking/views.py
+++ backend/parking/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
 
-# Create your views here.
+class BaseViewSet(viewsets.GenericViewSet, 
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin
+):
+  # Base viewset for table attributes containing the create, read and update actions but not delete.
+
+  def perform_create(self, serializer): 
+    """Create a new object"""
+    if(serializer.is_valid()):
+        content = serializer.save()
+        return Response(content, status=status.HTTP_201_CREATED)
+    else:
+        content = serializer.errors
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+  
+  def retrieve(self, request, *args, **kwargs):
+    return super().retrieve(request, *args, **kwargs)
+  
+  # disable full object updates so that the requests would always pass through the partial update function
+  def update(self, request, *args, **kwargs):
+    return Response(status=status.HTTP_403_FORBIDDEN)
